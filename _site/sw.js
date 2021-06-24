87400a210bc752b162d7d0546b89b8915b0bedd7
Ray CW <raywinardi@gmail.com>
2021-06-24 07:47:04 -0400

    fixed pages-deploy

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210624.0737';
+const cacheName = 'chirpy-20210624.0746';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
