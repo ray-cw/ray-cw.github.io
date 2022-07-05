fb92c7d2612dcaf1f16533ca6cc192e69e08cfcf
ray-cw <raywinardi@gmail.com>
2022-07-05 16:44:40 -0400

    IHjsfauksdhfhbjkasd

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -55,13 +55,6 @@ build() {
   JEKYLL_ENV=production bundle exec jekyll b -d "$SITE_DIR$_baseurl" --config "$_config"
 }
 
-test() {
-  bundle exec htmlproofer \
-    --disable-external \
-    --check-html \
-    --allow_hash_href \
-    "$SITE_DIR"
-}
 
 resume_site_dir() {
   if [[ -n $_baseurl ]]; then
@@ -119,7 +112,6 @@ deploy() {
 main() {
   init
   build
-  test
   resume_site_dir
 
   if $_opt_dry_run; then
